50b61af46a8582522f441f3e10c9fcaf86e2d7c5
Hsun-Yu Kuo <kuohsu@oregonstate.edu>
2023-04-13 20:43:06 -0700

    this is the final version of the ASCII art generator

--- asciiArt.js
+++ asciiArt.js
@@ -1,1 +1,23 @@
-var command_l = process.argv[2];
+const figlet = require('figlet');
+
+const input = process.argv[2];
+
+//check input text
+if (input) {
+
+  figlet(input, function(error, text) {
+
+    if (error) {
+
+      console.log('Error occurred... try again');
+      return;
+
+    }
+
+    console.log(text);
+  });
+
+} else {
+
+  console.log('No input found... try again');
+}
